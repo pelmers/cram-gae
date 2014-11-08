597a14415990fd2b859a67672bd039b3906c5e78
Peter Elmers <peter.elmers@yahoo.com>
2014-11-08 10:25:15 -0600

    make the buttons work

--- static/js/main.js
+++ static/js/main.js
@@ -3,31 +3,72 @@
         codeMirror = CodeMirror.fromTextArea(textArea, {
             mode: "javascript",
         }),
-        submitButton = document.getElementById("submitCode");
+        selectedShape = null;
 
-    submitButton.addEventListener('click', function() {
+    // Post the code string to /post/language with specified shape and ratio.
+    // Once we have the response, call callback(response)
+    function postCode(language, code, ratio, shape, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST","/post/"+language, true);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                callback(xhr.response);
+            }
+        };
+        xhr.send(JSON.stringify({
+            Shape: shape,
+            Ratio: ratio,
+            Contents: code,
+        }));
+    }
+
+    function getSelectedShape() {
+        return selectedShape;
+    }
+
+    function setSelectedShape(shapeName) {
+        document.getElementById(shapeName).classList.add('selected');
+        if (selectedShape !== null)
+            document.getElementById(selectedShape).classList.remove('selected');
+        selectedShape = shapeName;
+    }
+    setSelectedShape("rect");
+
+    function getRatio() {
+        return parseFloat(document.getElementById("ratioInput").innerHTML.slice(7));
+    }
+
+    // bind event listeners
+    var shapeDivs = document.getElementById("shapeSelect").children;
+    for (var i = 0; i < shapeDivs.length; i++) {
+        (function(id) {
+            document.getElementById(id).addEventListener('click', function() {
+                setSelectedShape(id);
+            });
+        })(shapeDivs[i].getAttribute("id"));
+    }
+
+    document.getElementById("ratioInput").addEventListener('wheel', function(e) {
+        function sign(val) {
+            return (val >= 0)?1:-1;
+        }
+        this.innerHTML = "Ratio: " + Math.max(0.05,
+                getRatio() + -0.05*sign(e.deltaY)).toFixed(2);
+    });
+
+    document.getElementById("submitCode").addEventListener('click', function() {
         var code = codeMirror.getValue(),
-            ratio = document.getElementById("ratioInput").value,
-            shape = document.getElementById("shapeSelect").value,
-            xhr = new XMLHttpRequest();
+            ratio = getRatio(),
+            shape = getSelectedShape();
         if (shape == "") {
             // just default to rectangle
             shape = "rect";
         }
         // eventually this will pick a specific endpoint based on langauge
         // but right now only js is supported
-        xhr.open("POST","/post/js", true);
-        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4) {
-                // replace body of codemirror with the response
-                codeMirror.setValue(xhr.response);
-            }
-        };
-        xhr.send(JSON.stringify({
-            Shape: shape,
-            Ratio: parseFloat(ratio),
-            Contents: code,
-        }));
+        postCode("js", code, ratio, shape, function(response) {
+            codeMirror.setValue(response);
+        });
     });
 })();
